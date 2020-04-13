3d779dc4b754cc5dc44d843f35bb157773d795ec
James Clawson <jmclawson@users.noreply.github.com>
2020-04-13 09:09:12 -0500

    Update weatherfrontman-bot.py
    Add weather detail, improve some phrasing/spacing in sentences

--- weatherfrontman-bot.py
+++ weatherfrontman-bot.py
@@ -10,7 +10,8 @@ def getresponse(locale):
 
     # Set them up as parameters for the query
     params = {'q' : combined,
-          'appid' : my_keys.WEATHER
+              'appid' : my_keys.WEATHER,
+              'units' : 'imperial'
       }
 
     # Pass the GET request with the right parameters, then parse the results
@@ -21,11 +22,22 @@ def getresponse(locale):
     weather_icon = weather_parsed['weather'][0]['icon']
     weather_id = weather_parsed['weather'][0]['id']
     
-    #weather_dictionary = {"01d":"u'\U00001F31E'", 
-    #                     "01n":"u'\U00001F31D'", 
-    #                     "04d":"u'\U00002601'"}
-    #for key in weather_dictionary.keys():
-    #    weather_icon = weather_icon.replace(key, weather_dictionary[key])
+    # Add details
+    weather_feels = "It feels like " + str(weather_parsed['main']['feels_like']) + "°F."
+    weather_low = "Today's low: " + str(weather_parsed['main']['temp_min']) + "°F."
+    weather_hi = "Today's high: " + str(weather_parsed['main']['temp_max']) + "°F."
+    weather_humid = "Humidity is at " + str(weather_parsed['main']['humidity']) + "%."
+    weather_most = "It currently feels " + str(weather_parsed['main']['feels_like']) + "°F, with a low of " + str(weather_parsed['main']['temp_min']) + ", a high of " + str(weather_parsed['main']['temp_max']) + ", and humidity at " + str(weather_parsed['main']['humidity']) + "%."
+    
+    # Choose detail
+    weather_detail = random.choice([weather_feels,
+                                    weather_low,
+                                    weather_hi,
+                                    weather_humid,
+                                    weather_most])
+    
+        
+    just_description = weather_description
     
     weather_emoji = ""  
     if weather_icon == "01n":# clear-night
@@ -91,16 +103,60 @@ def getresponse(locale):
     song_title = song_postparsed[chosen_song]['result']['full_title']
     
     # Randomize some results
-    my_verb = random.choice(["jamming to ", "playing ", "rockin' out to ", "blasting ", "singing along with "])
-    my_prep = random.choice([" in ", " over in ", " down in ", " up in ", " all over ", " all around "])
-    my_explain = random.choice(["(They've obviously got ", "(They must have ", "(It seems they have ", "(They have ", "(It's because of the ", "(It's all because of the "])
-    my_end = random.choice([" there.)", ".)" , ". "+ u'\U0001F3B8' +")"])
+    my_starter = random.choice(["They're ",
+                                "Sounds like they're ",
+                                "Folks are ",
+                                "Everyone's "])
     
-    alt1_begin = random.choice(["If I had so much ", "If I had their "])
-    alt1_end = random.choice([", I'd do the same!", ", that's all I'd want to hear, too!"])
+    my_verb = random.choice(["jamming to ",
+                             "playing ",
+                             "rockin' out to ",
+                             "blasting ",
+                             "singing along with ",
+                             "turning up the volume for ",
+                             "dancing to ",
+                             "shredding to ",
+                             "grooving to ",
+                             "boogieing to ",
+                             "bumpin' to ",
+                             "getting life from "])
+    my_prep = random.choice([" in ",
+                             " over in ",
+                             " down in ",
+                             " up in ",
+                             " all over ",
+                             " all around ",
+                             " up and down the streets in "])
+    my_explain = random.choice(["(They've obviously got ",
+                                "(They must have ",
+                                "(It seems they have ",
+                                "(They have ",
+                                "(It's because of the ",
+                                "(It's all because of the "])
+    my_end = random.choice([" there.)",
+                            ".)" ,
+                            ". "+ u'\U0001F3B8' +")",
+                            ". "+ u'\U0001FA95' +")",])
     
-    alt2_begin = random.choice (["With all that ", "With so much "])
-    alt2_end = random.choice([", who could blame them?!", ", you'd do the same!"])
+    alt1_begin = random.choice(["Had I ",
+                                "If I had ",
+                                "You know, if I had "])
+    alt1_end = random.choice([", I'd do the same!",
+                              ", that's all I'd want to hear, too!",
+                              ", that'd be my soundtrack, too!",
+                              ", I'd be " + my_verb + "it, too!"])
+    
+    alt2_begin = random.choice (["For all the ",
+                                 "With the ",
+                                 "After seeing ",
+                                 "With their ",
+                                 "Considering their "])
+    alt2_end = random.choice([", who could blame them?!",
+                              ", you'd do the same!",
+                              ", I don't blame them!",
+                              ", there's no better music!",
+                              ", it's the perfect jam!",
+                              ", you'd be " + my_verb + "it, too!"])
     
     # add an emoji
     weather_description += " "
@@ -108,13 +164,13 @@ def getresponse(locale):
     
     # Return the results, both unhelpful and helpful
     # The .split method divides the string at the comma and returns everything before it
-    sentence_one = "They're " + my_verb + song_title + my_prep + locale.split(', ', 1)[0] + "!"
+    sentence_one = my_starter + my_verb + song_title + my_prep + locale.split(', ', 1)[0] + "!"
     sentence_2one = my_explain + weather_description + my_end
-    sentence_2two = alt1_begin + weather_description + alt1_end + weather_emoji + u'\U0001F468\U0000200D\U0001F3A4'
+    sentence_2two = alt1_begin + just_description + alt1_end + weather_emoji + u'\U0001F468\U0000200D\U0001F3A4'
     sentence_2three = alt2_begin + weather_description + weather_emoji + weather_emoji + alt2_end + u'\U0001F3B8'
     sentence_two = random.choice([sentence_2one, sentence_2two, sentence_2three])
 
-    sentence = sentence_one + " " + sentence_two
+    sentence = sentence_one + " " + sentence_two + " " + weather_detail
     return sentence
 
 # Get the authentication keys from my_keys.py
